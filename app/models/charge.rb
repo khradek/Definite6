a3e83b9749578b79321f7fbf41a51a7cc169884a
Kevin Hradek <khradek@gmail.com>
2016-09-11 11:46:40 -0400

    update to receipt and update card

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -7,16 +7,16 @@ class Charge < ActiveRecord::Base
       id: id,
       product: "Definite6",
       company: {
-        name: "Definite6, Inc.",
-        address: "37 Great Jones\nFloor 2\nNew York City, NY 10012",
+        name: "Definite6.com",
+        address: "PO Box 58002\nWaxhaw, NC 28173",
         email: "admin@definite6.com",
-        logo: Rails.root.join("app/assets/images/creative/portfolio/football.jpg")
+        logo: Rails.root.join("app/assets/images/creative/portfolio/Logo.png")
       },
       line_items: [
         ["Date",           created_at.strftime("%m/%d/%Y").to_s],
         ["Account Billed", "#{user.email}"],
         ["Product",        "Definite6"],
-        ["Amount",         "$#{amount / 120}.00"],
+        ["Amount",         "$#{amount / 100}.00"],
         ["Charged to",     "#{card_brand} (**** **** **** #{card_last4})"],
       ],
     )
