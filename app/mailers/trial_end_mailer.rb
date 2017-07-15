55d438ba7dc6c5494346b341a12a930870748150
Kevin Hradek <khradek@gmail.com>
2017-07-15 16:05:14 -0400

    update to webhook

--- app/mailers/trial_end_mailer.rb
+++ app/mailers/trial_end_mailer.rb
@@ -2,8 +2,7 @@ class TrialEndMailer < ApplicationMailer
 
   def trial_end(user)
     @user = user
-    mail(to: "kevin@definite6.com", subject: "Your Definite6.com Trial") 
-    #mail(to: user.email, subject: "Your Definite6.com Trial")
+    mail(to: user.email, subject: "Your Definite6.com Trial")
   end
 
 end
