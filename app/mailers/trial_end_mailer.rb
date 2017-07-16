c0fc27f0641f73127601ade78e7196a37dbd3b9d
Kevin Hradek <khradek@gmail.com>
2017-07-16 10:45:10 -0400

    updates to trial expiration email

--- app/mailers/trial_end_mailer.rb
+++ app/mailers/trial_end_mailer.rb
@@ -2,6 +2,7 @@ class TrialEndMailer < ApplicationMailer
 
   def trial_end(user)
     @user = user
+    attachments.inline['Logo.png'] = File.read("app/assets/images/creative/portfolio/Logo.png")
     mail(to: user.email, subject: "Your Definite6.com Trial")
   end
 
