cfe74b2c09e28239c1215e7d4c2680c66cf437ca
Kevin Hradek <khradek@gmail.com>
2017-07-27 16:19:12 -0400

    adjusted number of threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2) #was 2
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 3) #was 5
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5) #was 5
 threads threads_count, threads_count
 
 preload_app!
